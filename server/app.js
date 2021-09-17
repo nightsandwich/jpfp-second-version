5a77c23ad7f02768ed845d4596c5ebccf927843e
Corinne Tinacci <corinne.tinacci@gmail.com>
2021-09-17 18:07:59 -0400

    heroku

--- server/app.js
+++ server/app.js
@@ -15,6 +15,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
 }); 
 
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 app.get('/api/campuses', async(req, res, next) => {
   try{
     res.send(await Campus.findAll({
--- server/index.js
+++ server/index.js
@@ -1,3 +1,11 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: 'postgres://zfpsoepnirdjev:efdb1ef78e26c935000c4eb68b9b46650efa6fe49546c7757a482d60d2fff576@ec2-34-227-120-94.compute-1.amazonaws.com:5432/d50ift7h6te4tg',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 const port = process.env.PORT || 3000;
 const app = require('./app');
 
