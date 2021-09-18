93d45729151293a1c484faedffa3bd0530668fc7
Corinne Tinacci <corinne.tinacci@gmail.com>
2021-09-18 12:20:09 -0400

    changed Home

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Home = () => {
+const Home = ({campuses, students}) => {
     return (
     <div>
         <div className='home'>
             <h1>Important</h1>
             <h1>Database</h1>
             <h1>Of</h1>
-            <h1>Campuses</h1>
+            <h1>{campuses.length} Campuses</h1>
             <h1>And</h1>
-            <h1>Students</h1>
+            <h1>{students.length} Students</h1>
         </div>
     </div>
     )
 }
 
-export default Home;
+export default connect(({campuses, students}) => ({campuses, students}))(Home);
